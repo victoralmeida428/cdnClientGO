9f5a7f203233695614f4448b1841a20f0087560d
victorgomes <victor.almeida@unigranrio.br>
2026-03-02 10:22:57 -0300

    refactor test for criptografia to handle encoding and decoding in a single line

--- cripto/cripto_test.go
+++ cripto/cripto_test.go
@@ -11,8 +11,8 @@ func TestCriptografia(t *testing.T) {
 	crip := New()
 
 	msg := "1792"
-	msgCripto := crip.Encode(msg)
-	msgDecode := crip.Decode(msgCripto)
+	msgCripto, _ := crip.Encode(msg)
+	msgDecode, _ := crip.Decode(msgCripto)
 	fmt.Printf("Original: %s\nCriptografado: %s\nDecode: %s\n", msg, msgCripto, msgDecode)
 	assert.Equal(t, msg, msgDecode)
 }
